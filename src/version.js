53bbd50514bab01c37f86c019417921d1bfeb9f6
Stefano Novelli <murdercode@gmail.com>
2025-06-20 12:11:16 +0200

    chore: release v0.2.16

--- src/version.js
+++ src/version.js
@@ -1,3 +1,3 @@
 // Questo file viene generato automaticamente durante il build
 // Contiene la versione corrente dell'applicazione
-export const APP_VERSION = '0.2.15';
+export const APP_VERSION = '0.2.16';
