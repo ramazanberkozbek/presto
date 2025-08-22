e574103df7027d1d7fb54459511ae6e376b1e797
Stefano Novelli <murdercode@gmail.com>
2025-08-22 15:08:53 +0200

    refactor: clean up knownThemes declaration and remove deprecated version-global.js

--- src/version-global.js
+++
@@ -1,4 +0,0 @@
-// Rende APP_VERSION disponibile globalmente per script non-module
-// Questo file viene generato automaticamente durante il build
-// Contiene la versione corrente dell'applicazione
-window.APP_VERSION = '0.3.10';
